58fbc344d3ae1564326f4ceebc9df31a685d687e
Nayan Parmar <nayanparmar@Nayans-MBP.hsd1.ca.comcast.net>
2021-12-31 16:23:25 -0800

    test case for Regular items decrease by 1 for sell_in and quality

--- test/test.js
+++ test/test.js
@@ -1,7 +1,13 @@
+const { it } = require('@jest/globals');
+const { update_quality, items, Item } = require('../src/gilded_rose');
+
 describe("Gilded Rose", function() {
 
-    it("should do something", function() {
-      update_quality();
-    });
+    it('Regular items decrease by 1 for sell_in and quality', () => {
+        const item = [new Item('+5 Dexterity Vest', 10, 20)];
+        update_quality(item);
+        expect(item[0].sell_in).toEqual();
+        expect(item[0].quality).toEqual();
+      });
   
   });
