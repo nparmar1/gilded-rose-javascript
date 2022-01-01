272141526e1b77eca68479a4a6f24bc3ee90ea49
Nayan Parmar <nayanparmar@Nayans-MBP.hsd1.ca.comcast.net>
2021-12-31 16:56:14 -0800

    wrote test case for Backstage passes quality increases by 1 as sell_in is greater than 10

--- test/test.js
+++ test/test.js
@@ -45,6 +45,12 @@ describe("Gilded Rose", function() {
         expect(item[0].quality).toEqual(80);
     });
   
+    it('Backstage passes\' quality increases by 1 as sell_in is greater than 10', () => {
+        const item = [new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20)];
+        update_quality(item);
+        expect(item[0].sell_in).toEqual();
+        expect(item[0].quality).toEqual();
+    });
 
     
   });
