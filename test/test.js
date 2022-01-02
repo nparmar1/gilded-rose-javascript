ea7ad67bfc13ac55f08bcfe31dfa18f4b9e8f4aa
Nayan Parmar <nayanparmar@Nayans-MBP.hsd1.ca.comcast.net>
2022-01-02 14:21:00 -0800

    test case passed

--- test/test.js
+++ test/test.js
@@ -76,8 +76,8 @@ describe("Gilded Rose", function() {
     it('Backstage passes quality increases by 3 when sell_in is less than 5', () => {
         const item = [new Item('Backstage passes to a TAFKAL80ETC concert', 4, 15)];
         update_quality(item);
-        expect(item[0].sell_in).toEqual();
-        expect(item[0].quality).toEqual();
+        expect(item[0].sell_in).toEqual(3);
+        expect(item[0].quality).toEqual(18);
     });
 
     
